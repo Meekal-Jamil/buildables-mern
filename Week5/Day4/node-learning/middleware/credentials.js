28f3e175dc53a6fa2d3d27527a3342703977613e
Meekal-Jamil <meekalstudent@gmail.com>
2025-09-22 20:47:34 +0500

    Week5 tasks in .env write PORT, MONGODB_URI, TOKENS to run it properly

---
+++ Week5/Day4/node-learning/middleware/credentials.js
@@ -0,0 +1,11 @@
+const whitelist = ['https://www.yoursite.com', 'http://127.0.0.1:5500', 'http://localhost:3500'];
+
+const credentials = (req, res, next) => {
+    const origin = req.headers.origin;
+    if (whitelist.includes(origin)) {
+        res.header('Access-Control-Allow-Credentials', true);
+    }
+    next();
+}
+
+module.exports = credentials;
